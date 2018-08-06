2a1a05ce4f46569a7c9464549ad701978bd2fb34
aaron mayhew <aaronmayhew@aarons-MacBook-Pro-2.local>
2018-08-06 16:35:08 -0500

    setting up the server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+var JsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
+app.use(bodyParser.raw({ type: 'application/vnd.cumstom-type' }));
+
+app.use(bodyParser.text({ type: 'text/html' }));
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
